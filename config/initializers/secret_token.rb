ba0dbe1a01fb794fa01507b293f7005f05fb2404
Oli James <olivermarkjames@gmail.com>
2012-02-23 03:39:33 +1300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brass::Application.config.secret_token = 'ded812b0eba9a69e22a1caa7fbe04ee6978f255ecbe7aa637010705d0486f09795a4c870df6e90bd33b2ef1b22ca0406e96537d19a5d2eaaeec3804c414d51fd'
